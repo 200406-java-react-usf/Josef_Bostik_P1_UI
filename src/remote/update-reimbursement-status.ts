5ae4bd9da8459e8bef4af049685f9407e3ffc03c
jbostik <ebiven1563@gmail.com>
2020-05-15 15:07:29 -0400

    Implemented a manager reimbursement component, which allows a finance manager to view all reimbursements, approve or deny reimbursements, and filter reimbursements by type/status

---
+++ src/remote/update-reimbursement-status.ts
@@ -0,0 +1,16 @@
+import { revaboardsClient } from "./revaboards-client";
+import { Reimbursement } from "../models/reimbursements";
+
+export async function updateReimbursementStatus(reimb: Reimbursement, status: number, authId: number) {
+    try {
+        let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let dateTime = date+' '+time;
+        //not the most elegant solution, but it works for the time being
+        let response = await revaboardsClient.patch(`/reimbursements/${reimb.id}`, {amount: reimb.amount, submitted: reimb.submitted, resolved: dateTime, description: reimb.description, receipt: reimb.receipt, author: reimb.author, resolver: authId, reimb_status_id: status, reimb_type_id: reimb.reimb_type_id});
+        return await response;
+    } catch (e) {
+        return e.response.status
+    }
+}
