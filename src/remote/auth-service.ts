14fb3e2c940109b29448752ddcc61bb736547582
jbostik <ebiven1563@gmail.com>
2020-05-18 21:15:58 -0400

    cleaned up a few things, no functional changes

--- src/remote/auth-service.ts
+++ src/remote/auth-service.ts
@@ -2,5 +2,6 @@ import { revaboardsClient } from "./revaboards-client";
 
 export async function authenticate(username: string, password: string) {
     let response = await revaboardsClient.post('/auth', {username, password});
+    //console.log(response.statusText);
     return await response;
 }
