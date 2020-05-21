e74118c2659105e0ba7d1cf3797fb5e608e6e2c3
jbostik <ebiven1563@gmail.com>
2020-05-21 17:52:16 -0400

    added some tests, 50% test coverage

--- src/remote/p1-client.ts
+++ src/remote/p1-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const p1Client = axios.create({
-    //baseURL: 'http://project1-env-1.eba-mssfhvpm.us-east-2.elasticbeanstalk.com/',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://project1-env-1.eba-mssfhvpm.us-east-2.elasticbeanstalk.com/',
+    //baseURL: 'http://localhost:8080',
     headers: {
         'Content-Type': 'application/json'
     },
